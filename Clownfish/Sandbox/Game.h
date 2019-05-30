88cdd80b40c8288d1af010fdb4272dfb5aac528a
Unknown <jannik.lietz@zeiteffekt.info>
2019-05-30 15:39:46 +0200

    Restored Test Layout

--- Clownfish/Sandbox/Game.h
+++ Clownfish/Sandbox/Game.h
@@ -5,50 +5,63 @@ using namespace clownfish;
 using namespace graphics;
 using namespace input;
 using namespace maths;
-//using namespace audio;
+using namespace audio;
 
 class Game : public Clownfish
 {
 
 private:
-	Window * window;
-	BatchRenderer2D* renderer;
-	Layer* layer;
+	Window* window;
 	Sprite* logo;
-
-
+	Layer* layer;
+	Label* fps;
+	Label* ups;
+	BatchRenderer2D* renderer;
 public:
 	Game() {}
 
 	// On Begin
 	void init() override
 	{
-		window = createWindow("Sandbox", 960, 540, false);
+		window = createWindow("Game", 960, 540, false);
 
 		renderer = new BatchRenderer2D();
 		layer = new Layer(renderer, new Shader("shaders/basic.vert", "shaders/basic.frag"), maths::mat4::othographic(-16, 16, -9.0f, 9.0f, -1.0f, 1.0f));
 
-		logo = new Sprite(0, 0, 10, 8, "logo.png");
+
+		fps = new Label("5000", -15.5f, 7.5f, "mario", 42, 0xff0000ff);
+		ups = new Label("60", 14.0f, 7.5f, "arial", 42, 0xff0000ff);
+
+
+		logo = new Sprite(-6, 0, 10, 8, "logo.png");
+		Sprite* wall = new Sprite(0, -3, 2, 6, 0xff00ffff);
+
+		wall->setColision(true);
+
+		logo->setColision(true);
+		layer->add(new Label("Welcome to", -9.5f, 5, "mario", 80, 0xff00ffff));
+		layer->add(new Label("Clownfish!", -8, 3, "mario", 80, 0xff00ffff));
+		AudioManager::add(new AudioClip("background", "zelda.wav"));
+
+		layer->add(wall);
 		layer->add(logo);
-		Font* font = new Font("arial", "fonts/arial", 45);
-		layer->add(new Label("Janik stinkt!", 0, 5, FontManager::get("arial"), Color::YellowGreen));
-
-
+		layer->add(fps);
+		layer->add(ups);
+		AudioManager::get("background")->loop();
 	}
 
 	// Once per Second
 	void tick() override
 	{
-
-
-
+		std::cout << std::to_string(getFPS()) << " , " << std::to_string(getUPS()) << std::endl;
+		fps->text = std::to_string(getFPS());
+		ups->text = std::to_string(getUPS());
 	}
 
 
 	//60 Times per Second
 	void update() override
 	{
-
 		float x = Input::GetAxis(HORIZONTAL);
 		float y = Input::GetAxis(VERTICAL);
 
@@ -60,12 +73,15 @@ public:
 
 		logo->translate(vec3(x * 0.5f, y * 0.5f, 0));
 
+
 	}
 
 	//As fast as possible
 	void render() override
 	{
+		glClearColor(0, 0, 0.8f, 1);
 		layer->render();
+		AudioManager::update();
 	}
 
 };
